4afad5a38e07eb96a6da49063bfa4c9738daed20
erfanelahi <erfan.elahi@gmail.com>
2024-04-04 03:00:08 +0600

    A Food Should Have Only One Type

--- Services/Restaurant/Restaurant.API/Entities/Food.cs
+++ Services/Restaurant/Restaurant.API/Entities/Food.cs
@@ -8,7 +8,7 @@ namespace Restaurant.API.Entities
         public required int Id { get; init; }
         public required string Name { get; init; }
         public required double Price { get; init; }
-        public required List<FoodType> Types { get; init; }
+        public required FoodType Type { get; init; }
 
         public string? Description { get; init; }
         public string? ImageUrl { get; init; }
